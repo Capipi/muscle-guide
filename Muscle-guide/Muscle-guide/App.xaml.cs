b4958d374668e799ee0c7731e511caa08422d104
Samuel <elmegalodondeagua@gmail.com>
2025-04-08 22:03:11 +0200

    Primer commit con el proyecto creado vacío

---
+++ Muscle-guide/Muscle-guide/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Muscle_guide
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
