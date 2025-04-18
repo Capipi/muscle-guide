2fc5d17f7db041ae37fc172310bb6e64e433f21b
Samuel <elmegalodondeagua@gmail.com>
2025-04-18 18:33:59 +0200

    Pruebas curso básico

---
+++ Muscle pruebas/Muscle preubas/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Muscle_preubas
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //MostrarEtiqueta();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Hola mundo desde WPF");
+            var textEnvio = "Hola mundo desde WPF";
+            var nuevaVentana = new Window1(textEnvio);
+            nuevaVentana.Show();
+        }
+
+        //private void MostrarEtiqueta()
+        //{
+        //    lblTitle.Content = "Hola WPF";
+        //    lblTitle.FontSize = 50;
+        //    lblTitle.HorizontalAlignment = HorizontalAlignment.Center;
+        //    lblTitle.VerticalAlignment = VerticalAlignment.Center;
+        //}
+    }
+}
---
+++ Muscle pruebas/Muscle preubas/Window1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Muscle_preubas
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1(string textoEnviado)
+        {
+            InitializeComponent();
+            lblTitle.Content = textoEnviado;
+            lblTitle.FontSize =50;
+        }
+    }
+}
